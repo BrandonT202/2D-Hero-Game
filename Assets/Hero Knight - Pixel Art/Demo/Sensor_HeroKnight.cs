2e66389592c08a7483f6b240ce9e95e8d7b05537
BrandonT202 <brandon.t202@gmail.com>
2022-12-16 15:17:53 +0000

    Initial commit

---
+++ Assets/Hero Knight - Pixel Art/Demo/Sensor_HeroKnight.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Sensor_HeroKnight : MonoBehaviour {
+
+    private int m_ColCount = 0;
+
+    private float m_DisableTimer;
+
+    List<GameObject> m_collided;
+
+    private void OnEnable()
+    {
+        m_ColCount = 0;
+        m_collided = new List<GameObject>();
+    }
+
+    public bool State()
+    {
+        if (m_DisableTimer > 0)
+            return false;
+        return m_ColCount > 0;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        m_ColCount++;
+        m_collided.Add(other.gameObject);
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        m_ColCount--;
+        m_collided.Remove(other.gameObject);
+    }
+
+    void Update()
+    {
+        m_DisableTimer -= Time.deltaTime;
+    }
+
+    public void Disable(float duration)
+    {
+        m_DisableTimer = duration;
+    }
+
+    public GameObject[] GetAllColliders()
+    {
+        return m_collided.ToArray();
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D body;
+
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private float jumpSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var horizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+
+        if (horizontalInput > 0.01f)
+        {
+            transform.localScale = Vector3.one;
+        }
+        else if (horizontalInput < -0.01f)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            body.velocity = new Vector2(body.velocity.x, jumpSpeed);
+        }
+    }
+}
